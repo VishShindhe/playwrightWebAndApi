f897c63282eac078205fd358a9d6bdecd0e3a07c
VishShindhe <vishwa.shindhe@gmail.com>
2024-05-05 13:04:29 +0530

    initialise

---
+++ e2e/tests/addEmployee/addEmployee.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+import { Env } from "../../frameworkConfig/env";
+
+test("Add Employee", async ({ page }) => {
+  await page.goto(Env.BASE_URL);
+  await page.getByPlaceholder("Username").fill(Env.USERNAME);
+  await page.getByPlaceholder("Password").fill(Env.PASSWORD);
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByRole("link", { name: "PIM" }).click();
+  await page.getByRole("link", { name: "Add Employee" }).click();
+  await page.getByPlaceholder("First Name").click();
+  await page.getByPlaceholder("First Name").fill("test1");
+  await page.getByPlaceholder("Middle Name").fill("t");
+  await page.getByPlaceholder("Last Name").fill("tester1");
+  await page.locator("form").getByRole("textbox").nth(4).fill("1223");
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText(/Successfully Saved/i)).toBeVisible();
+});
---
+++ playwright.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, devices } from "@playwright/test";
+import { config } from "dotenv";
+
+//staging ==> .env.staging
+//to run with specific environment from cmd line
+// ENVIRONMENT=staging npx playwright test --headed
+if (process.env.ENVIRONMENT) {
+  console.log("ENIRONMENT: ", process.env.ENVIRONMENT);
+  config({
+    path: `.env.${process.env.ENVIRONMENT}`,
+    override: true,
+  });
+} else {
+  config();
+}
+
+export default defineConfig({
+  testDir: "./e2e/tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+
+  retries: process.env.CI ? 2 : 0,
+
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: "html",
+
+  use: {
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
